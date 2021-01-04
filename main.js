50e857301036b2b7f86c483f0d5c40b462aa130b
natchez <natchezzz@hotmail.com>
2021-01-04 11:31:08 +0100

    fin juste bug MAj exo 1

---
+++ main.js
@@ -0,0 +1,30 @@
+        // exo 1
+// let tab = ['pomme', 'fraise', 'melon', 'citron']
+// console.log(tab)
+// tab.shift()
+// console.log(tab)
+// tab.push('banane')
+// console.log(tab)
+// tab.unshift("kiwy")
+// console.log(tab)
+// let maj = tab[1].toUpperCase()
+// console.log(tab[1].toUpperCase())
+// console.log(tab)
+// let tabv2 = tab.map(tab => tab[1].toUpperCase())
+// console.log(tabv2)
+// tab = tab.reverse()
+// console.log(tab)
+        // exo 2
+// let chiffres = [4, 5, 2, 1, 3]
+// console.log(chiffres)
+// chiffres.sort()
+// console.log(chiffres)
+        // exo 3
+let valentin = []
+console.log(valentin)
+valentin.push(1,2,3,4,5,6,7,8,9,10)
+console.log(valentin)
+let valentin2 = ['objet 1', 'objet 2', 'objet 3', 'objet 4', 'objet 5']
+console.log(valentin2)
+valentin2.splice(1,1)
+console.log(valentin2)
